bf455e82f70c561c419cabfe73abb19a011751fd
zyimm <zyimm@qq.com>
2021-07-27 15:46:25 +0800

    题库 分页

--- front/src/views/examManage.vue
+++ front/src/views/examManage.vue
@@ -1,35 +1,48 @@
 <template>
     <div>
-        <Form ref="formInline" :model="examSearch"
-              :label-width="88"
-              label-position="right" :rules="ruleInline" inline>
-            <FormItem label="考试名称:" prop="examName">
-                <Input type="text" v-model="examSearch.examName" placeholder="考试名称" clearable/>
-            </FormItem>
+        <Card style="margin: 10px 0">
+            <Form ref="formInline" :model="examSearch"
+                  :label-width="88"
+                  label-position="right" :rules="ruleInline" inline>
+                <FormItem label="考试名称:" prop="examName">
+                    <Input type="text" v-model="examSearch.examName" placeholder="考试名称" clearable/>
+                </FormItem>
 
-            <FormItem label="考试类型:" prop="examType">
-                <Select v-model="examSearch.examType"  filterable clearable>
-                    <Option v-for="item in examSearch.examTypeMap" :value="item.key" :key="item.key">{{ item.name }}</Option>
-                </Select>
-            </FormItem>
+                <FormItem label="考试类型:" prop="examType">
+                    <Select v-model="examSearch.examType" filterable clearable>
+                        <Option v-for="item in examSearch.examTypeMap" :value="item.key" :key="item.key">{{ item.name
+                            }}
+                        </Option>
+                    </Select>
+                </FormItem>
 
-            <FormItem label="考试状态:" prop="examStatus">
-                <Select v-model="examSearch.examType"  filterable clearable>
-                    <Option v-for="item in examSearch.examStatusMap" :value="item.key" :key="item.key">{{ item.name }}</Option>
-                </Select>
-            </FormItem>
+                <FormItem label="考试状态:" prop="examStatus">
+                    <Select v-model="examSearch.examStatus" filterable clearable>
+                        <Option v-for="item in examSearch.examStatusMap" :value="item.key" :key="item.key">{{ item.name
+                            }}
+                        </Option>
+                    </Select>
+                </FormItem>
 
-            <FormItem label="考试时间:" prop="examTimeRange">
-                <DatePicker type="datetimerange" v-model="examSearch.examTimeRange" format="yyyy-MM-dd"></DatePicker>
-            </FormItem>
-        </Form>
+                <FormItem label="考试时间:" prop="examTimeRange">
+                    <DatePicker type="datetimerange" v-model="examSearch.examTimeRange"
+                                format="yyyy-MM-dd"></DatePicker>
+                </FormItem>
+            </Form>
+        </Card>
 
-        <Card>
+        <Card ref="tableWrapper">
             <div slot="title">
-                <Button type="primary"  icon="ios-add-circle-outline"> 新增题库</Button>
+                <Button type="primary" icon="ios-add-circle-outline"> 新增题库</Button>
             </div>
             <div>
-                <Table border :columns="columns" :data="rows"></Table>
+                <Scroll :height="tableHeight" >
+                    <Table border :columns="columns" :data="rows"></Table>
+                </Scroll>
+            </div>
+
+            <div>
+                <Page :total="100" show-total show-sizer />
             </div>
         </Card>
 
@@ -43,15 +56,16 @@
                 examSearch: {
                     examName: '',
                     examType: 0,
+                    examStatus: 0,
                     examTypeMap: [
                         {
-                          key:1,
-                          name:"司法考试"
+                            key: 1,
+                            name: "司法考试"
                         }
                     ],
-                    examStatusMap:[{
-                        key:1,
-                        name:"进行中"
+                    examStatusMap: [{
+                        key: 1,
+                        name: "进行中"
                     }],
                     examTimeRange: []
                 },
@@ -102,7 +116,6 @@
                         key: 'action',
                         render: (h, params) => {
                             console.log(params)
-
                             return h('div', [
                                 h('Button', {
                                     props: {
@@ -152,8 +165,70 @@
                         province: 'Canada',
                         city: 'Ottawa',
                         zip: 100000
+                    },
+                    {
+                        name: 'John Brown',
+                        age: 18,
+                        address: 'New York No. 1 Lake Park',
+                        province: 'America',
+                        city: 'New York',
+                        zip: 100000
+                    },
+                    {
+                        name: 'John Brown',
+                        age: 18,
+                        address: 'New York No. 1 Lake Park',
+                        province: 'America',
+                        city: 'New York',
+                        zip: 100000
+                    },
+                    {
+                        name: 'John Brown',
+                        age: 18,
+                        address: 'New York No. 1 Lake Park',
+                        province: 'America',
+                        city: 'New York',
+                        zip: 100000
+                    },
+                    {
+                        name: 'John Brown',
+                        age: 18,
+                        address: 'New York No. 1 Lake Park',
+                        province: 'America',
+                        city: 'New York',
+                        zip: 100000
+                    },
+                    {
+                        name: 'John Brown',
+                        age: 18,
+                        address: 'New York No. 1 Lake Park',
+                        province: 'America',
+                        city: 'New York',
+                        zip: 100000
+                    },
+                    {
+                        name: 'John Brown',
+                        age: 18,
+                        address: 'New York No. 1 Lake Park',
+                        province: 'America',
+                        city: 'New York',
+                        zip: 100000
+                    },
+                    {
+                        name: 'John Brown',
+                        age: 18,
+                        address: 'New York No. 1 Lake Park',
+                        province: 'America',
+                        city: 'New York',
+                        zip: 100000
                     }
                 ]
+            }
+        },
+        computed: {
+            tableHeight() {
+                console.log(document.body.offsetHeight);
+                return 320;
             }
         },
         methods: {
