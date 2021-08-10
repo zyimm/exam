e44838a13af31f8c31e256de61bb39ad9822590f
zyimm <zyimm@qq.com>
2021-08-10 16:18:01 +0800

    注释

--- backend/src/main/java/com/example/exam/ExamApplication.java
+++ backend/src/main/java/com/example/exam/ExamApplication.java
@@ -3,9 +3,12 @@ package com.example.exam;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 
+ * @author zyimm
+ */
 @SpringBootApplication
 public class ExamApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ExamApplication.class, args);
     }
