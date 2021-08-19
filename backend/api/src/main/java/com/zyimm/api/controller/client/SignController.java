8f9d6c716b6887fe811243b1990e52909036cd65
zyimm <zyimm@qq.com>
2021-08-19 14:51:04 +0800

    登录

---
+++ backend/api/src/main/java/com/zyimm/api/controller/client/SignController.java
@@ -0,0 +1,13 @@
+package com.zyimm.api.controller.client;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 登录
+ * 
+ * @author zyimm
+ */
+@RestController
+public class SignController {
+    
+}
