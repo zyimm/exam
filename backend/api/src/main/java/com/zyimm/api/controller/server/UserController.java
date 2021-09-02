6ef86890f2724045766c49baafc8190a002b7bee
zyimm <zyimm@qq.com>
2021-09-02 14:52:41 +0800

    xml

--- backend/api/src/main/java/com/zyimm/api/controller/server/UserController.java
+++ backend/api/src/main/java/com/zyimm/api/controller/server/UserController.java
@@ -1,5 +1,7 @@
 package com.zyimm.api.controller.server;
 
+import java.util.Map;
+
 import com.zyimm.common.request.UserRequest;
 import com.zyimm.common.response.Response;
 import com.zyimm.dao.entity.UserEntity;
@@ -8,8 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
 
 /**
  * @author zyimm
@@ -42,7 +42,6 @@ public class UserController {
      */
     @GetMapping("/user/user/{id}")
     public Response getMember(@PathVariable("id") Long id){
-    
         UserEntity result = userService.getUserInfoById(id);
         return Response.success(result);
     }
