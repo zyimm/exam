fec5abe957d703ee6156cf6facf5635355951879
zhouyangyang <zhouyangyang@hoge.cn>
2021-10-21 19:53:42 +0800

    插入数据

--- backend/api/src/main/java/com/zyimm/api/controller/server/UserController.java
+++ backend/api/src/main/java/com/zyimm/api/controller/server/UserController.java
@@ -3,12 +3,14 @@ package com.zyimm.api.controller.server;
 import java.util.Map;
 
 import com.zyimm.common.request.UserQueryRequest;
+import com.zyimm.common.request.UserRequest;
 import com.zyimm.common.response.Response;
 import com.zyimm.dao.dto.UserDto;
 import com.zyimm.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -22,10 +24,10 @@ public class UserController {
     private UserService  userService;
 
     /**
-     *  用户列表
-     * 
-     * @param UserQueryRequest
-     * @return
+     * 用户列表
+     *
+     * @param userQueryRequest 查询参数
+     * @return Response
      */
     @GetMapping("/user/users")
     public Response getMembers(UserQueryRequest userQueryRequest){
@@ -33,12 +35,23 @@ public class UserController {
         return Response.success(result);
     }
 
+    /**
+     * 新增用户
+     *
+     * @param userRequest 查询参数
+     * @return Response
+     */
+    @PostMapping("/user/user")
+    public Response insertMembers(UserRequest userRequest){
+        Map<String, Object> result = userService.insertUserForRequest(userRequest);
+        return Response.success(result);
+    }
 
     /**
      *  用户列表
-     * 
-     * @param userRequest
-     * @return
+     *
+     * @param id 会员id
+     * @return Response
      */
     @GetMapping("/user/user/{id}")
     public Response getMember(@PathVariable("id") Long id){
--- backend/service/src/main/java/com/zyimm/service/UserService.java
+++ backend/service/src/main/java/com/zyimm/service/UserService.java
@@ -14,37 +14,37 @@ public interface UserService {
      /**
      * 根据id获取会员信息
      * 
-     * @param id
+     * @param id 会员id
      * @return UserDto
      */
-    public UserDto getUserInfoById(Long id);
+     UserDto getUserInfoById(Long id);
 
 
     /**
      * 获取会员列表
      * 
-     * @param  userRequest
+     * @param  userRequest 请求参数
      * @return Map<String, Object>
      */
-    public Map<String, Object> getUserList(UserQueryRequest userRequest);
+    Map<String, Object> getUserList(UserQueryRequest userRequest);
 
 
     /**
      * 
      * 新增会员
      * 
-     * @param userEntity
-     * @return 
+     * @param userEntity 用户实体
+     * @return  String
      */
-    public String insertUser(UserEntity userEntity);
+    String insertUser(UserEntity userEntity);
 
     /**
      * 
      * 新增会员
      * 
-     * @param user
-     * @return 
+     * @param user 提交参数
+     * @return Map<String, Object>
      */
-    public Map<String, Object> insertUserForRequest(UserRequest user);
+    Map<String, Object> insertUserForRequest(UserRequest user);
     
 }
