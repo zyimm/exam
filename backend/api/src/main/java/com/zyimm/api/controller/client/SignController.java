d934a68a847c2aee1291f3f4b2762c945972e010
zyimm <zyimm@qq.com>
2021-11-05 14:57:48 +0800

    注入

--- backend/api/src/main/java/com/zyimm/api/controller/client/SignController.java
+++ backend/api/src/main/java/com/zyimm/api/controller/client/SignController.java
@@ -3,7 +3,6 @@ package com.zyimm.api.controller.client;
 import com.zyimm.common.request.SignRequest;
 import com.zyimm.common.response.Response;
 import com.zyimm.service.SignService;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,8 +19,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class SignController {
 
 
-    @Autowired
+
     private SignService signService;
+
+    @Autowired
+    public  void setSignService(SignService signService){
+        this.signService = signService;
+    }
     
 
     /**
